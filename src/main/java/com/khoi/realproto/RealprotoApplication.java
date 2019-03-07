19aed8433e9ef723c6ee19905d3617b27de67ef7
Khoi Nguyen Minh <intern.nmkhoi@tma.com.vn>
2019-03-07 17:27:54 +0700

    proto

---
+++ src/main/java/com/khoi/realproto/RealprotoApplication.java
@@ -0,0 +1,13 @@
+package com.khoi.realproto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealprotoApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RealprotoApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/khoi/realproto/RealprotoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.khoi.realproto;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RealprotoApplicationTests {
+
+  @Test
+  public void contextLoads() {
+  }
+
+}
